c51c25668742471e03a97f82db479654a1158435
Frito <malline@Frito-Kubuntu.(none)>
2019-09-18 08:46:44 -0500

    Updated storage to use named parameters and I/O serialization

--- src/repos/sqlite3/db.js
+++ src/repos/sqlite3/db.js
@@ -5,13 +5,15 @@ const { logger } = require('../../globals');
 
 const createOperationTableSql = `
 CREATE TABLE IF NOT EXISTS Operation (
-    id        TEXT PRIMARY KEY,
-    execution TEXT NOT NULL,
-    created   TEXT NOT NULL,
-    stateKey  TEXT NOT NULL,
-    status    TEXT NOT NULL,
-    input     TEXT,
-    output    TEXT,
+    id         TEXT PRIMARY KEY,
+    execution  TEXT NOT NULL,
+    created    TEXT NOT NULL,
+    stateKey   TEXT NOT NULL,
+    status     TEXT NOT NULL,
+    input      TEXT,
+    inputType  TEXT,
+    output     TEXT,
+    outputType Text,
   FOREIGN KEY (execution) REFERENCES Execution (id) ON DELETE CASCADE
 ) WITHOUT ROWID;
 `;
@@ -72,12 +74,14 @@ const getStateMachines = (db) => db.all('SELECT * FROM StateMachine').then((resu
 
 const createStateMachine = (db, id, name, definitionObject) => {
   const versionId = uuid.v4();
-  return db.run('INSERT INTO StateMachineVersion VALUES (?, ?)', [versionId, JSON.stringify(definitionObject)])
-    .then(() => db.run('INSERT INTO StateMachine VALUES (?, ?, ?)', [id, name, versionId]));
+  return db.run('INSERT INTO StateMachineVersion VALUES ($id, $definition)', { $id: versionId, $definition: JSON.stringify(definitionObject) })
+    .then(() => db.run('INSERT INTO StateMachine VALUES ($id, $name, $active_version)', { $id: id, $name: name, $active_version: versionId }));
 };
 
-const getStateMachine = (db, id) => db.get('SELECT * FROM StateMachine WHERE id = ?', [id])
-  .then((stateMachine) => db.get('SELECT * FROM StateMachineVersion WHERE id = ?', [stateMachine.active_version]).then((stateMachineVersion) => ({ stateMachine, stateMachineVersion }))).then((data) => ({
+const getStateMachine = (db, id) => db.get('SELECT * FROM StateMachine WHERE id = $id', { $id: id })
+  .then((stateMachine) => db.get('SELECT * FROM StateMachineVersion WHERE id = $id', { $id: stateMachine.active_version })
+    .then((stateMachineVersion) => ({ stateMachine, stateMachineVersion })))
+  .then((data) => ({
     ...data.stateMachine,
     definition: JSON.parse(data.stateMachineVersion.definition),
   }))
@@ -85,15 +89,34 @@ const getStateMachine = (db, id) => db.get('SELECT * FROM StateMachine WHERE id 
     logger.warn('Error during execution', err);
   });
 
-const createExecution = (db, id, versionId) => db.run('INSERT INTO Execution VALUES (?, ?, ?, ?)', [id, new Date().toISOString(), 'Pending', versionId]);
-const updateExecution = (db, id, status) => db.run('UPDATE Execution SET status = ? WHERE id = ?', [status, id]);
-const getExecution = (db, id) => db.get('SELECT * FROM Execution WHERE id = ?', [id]);
-const getStateMachineDefinitionForExecution = (db, id) => db.get('SELECT smv.definition FROM Execution AS e JOIN StateMachineVersion AS smv ON e.version = smv.id WHERE e.id = ?', [id]);
-const getDetailsForExecution = (db, id) => db.all('SELECT e.status AS executionStatus, o.* FROM Execution AS e JOIN Operation AS o ON e.id = o.execution WHERE e.id = ?', [id]);
+const createExecution = (db, id, versionId) => db.run('INSERT INTO Execution VALUES ($id, $created, $status, $version)', {
+  $id: id,
+  $created: new Date().toISOString(),
+  $status: 'Pending',
+  $version: versionId,
+});
+const updateExecution = (db, id, status) => db.run('UPDATE Execution SET status = $status WHERE id = $id', { $status: status, $id: id });
+const getExecution = (db, id) => db.get('SELECT * FROM Execution WHERE id = $id', { $id: id });
+const getStateMachineDefinitionForExecution = (db, id) => db.get('SELECT smv.definition FROM Execution AS e JOIN StateMachineVersion AS smv ON e.version = smv.id WHERE e.id = $executionId', { $executionId: id });
+const getDetailsForExecution = (db, id) => db.all('SELECT e.status AS executionStatus, o.* FROM Execution AS e JOIN Operation AS o ON e.id = o.execution WHERE e.id = $executionId', { $executionId: id });
 
-const createOperation = (db, id, executionId, stateKey, input) => db.run('INSERT INTO Operation VALUES (?, ?, ?, ?, ?, ?, NULL)', [id, executionId, new Date().toISOString(), stateKey, 'Pending', input]);
-const updateOperation = (db, id, state, output) => db.run('UPDATE Operation SET status = ?, output = ? WHERE id = ?', [state, output, id]);
-const getOperation = (db, id) => db.get('SELECT * FROM Operation WHERE id = ?', [id]);
+const createOperation = (db, id, executionId, stateKey, input) => db.run('INSERT INTO Operation VALUES ($id, $executionId, $created, $stateKey, $status, $input, $inputType, $output, $outputType)', {
+  $id: id,
+  $executionId: executionId,
+  $created: new Date().toISOString(),
+  $stateKey: stateKey,
+  $status: 'Pending',
+  $input: input,
+  $inputType: null,
+  $output: null,
+  $outputType: null,
+});
+const updateOperation = (db, id, status, output) => db.run('UPDATE Operation SET status = $status, output = $output WHERE id = $id', {
+  $status: status,
+  $output: output,
+  $id: id,
+});
+const getOperation = (db, id) => db.get('SELECT * FROM Operation WHERE id = $id', { $id: id });
 
 module.exports = {
   getDb,
